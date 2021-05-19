053e072eb72256f51ce54a8eb1eade29d29660b2
Luis Marcilio Braga <lbraga@pavlovmedia.com>
2021-05-19 09:35:23 -0300

    fix: js functions should return a string

--- scripts/SP.js
+++ scripts/SP.js
@@ -2,9 +2,9 @@ const jsFunctions = () => {
   const accessKey = "$$ACCESS_KEY$$";
   const isSimplifiedNf = document.querySelector("#conteudo_btnDetalhe");
   const isHome = document.querySelector("#conteudo_txtChaveAcesso");
-  const isCfe = document.querySelector("#conteudo_tabCfe")?.style?.backgroundColor == "rgb(200, 50, 50)";
-  const isEmitente = document.querySelector("#conteudo_tabEmitente")?.style?.backgroundColor == "rgb(200, 50, 50)";
-  const isProdutos = document.querySelector("#conteudo_tabProdutoServico")?.style?.backgroundColor == "rgb(200, 50, 50)";
+  const isCfe = document.querySelector("#conteudo_tabCfe")?.style?.backgroundColor === "rgb(200, 50, 50)";
+  const isEmitente = document.querySelector("#conteudo_tabEmitente")?.style?.backgroundColor === "rgb(200, 50, 50)";
+  const isProdutos = document.querySelector("#conteudo_tabProdutoServico")?.style?.backgroundColor === "rgb(200, 50, 50)";
   const data = {};
   if (isHome) {
       document.querySelector("#conteudo_txtChaveAcesso").value = accessKey;
@@ -26,7 +26,7 @@ const jsFunctions = () => {
       emitente: localStorage.getItem("emitente"),
       produtos: document.documentElement.outerHTML,
     };
-    return data;
+    return JSON.stringify(data);
   }
 };
 jsFunctions();
